fc60bfaa72e84f5a85858e94b264e792cd96fbc3
Rob Hess <robwhess@gmail.com>
2018-11-02 16:51:04 -0700

    Add code from 11/2 lecture.

---
+++ files.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+fs.readFile('test.js', 'utf-8', function (err, contents) {
+  if (err) {
+    throw err;
+  }
+  console.log(contents);
+});
+
+console.log("======= This comes after readFile()");
+
+var fileContents = fs.readFileSync('test.js', 'utf-8');
+console.log(fileContents);
+
+fs.writeFile(
+  'writtenFile.txt',
+  "This is what will be written into the file.\n",
+  'utf-8',
+  function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("== File successfully written");
+  }
+);
--- test.js
+++ test.js
@@ -18,6 +18,13 @@ var circumference = require('./circumference');
 console.log("== circumference(5):", circumference(5));
 console.log("== circumference(8):", circumference(8));
 
-var circle = require('./circle');
+var circle = require('./lib/circle');
 console.log("== circle.circumference(5):", circle.circumference(5));
 console.log("== circle.area(5):", circle.area(5));
+
+var figlet = require('figlet');
+figlet('CS 290!', function (err, data) {
+  if (data) {
+    console.log(data);
+  }
+});
